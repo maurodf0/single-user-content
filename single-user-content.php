e4ce116c0b9779b2086c0e6dccd97b7b3a7bf758
Mauro <maurodefalco96@gmail.com>
2024-06-29 17:42:41 +0200

    add meta_boxs

--- single-user-content.php
+++ single-user-content.php
@@ -52,7 +52,57 @@ function sucplugin_add_metaboxes() {
 add_action('add_meta_boxes', 'sucplugin_add_metaboxes');
 
 //add the HTML for the metabox
-function sucplugin_metabox_content() {?>
+function sucplugin_metabox_content() {
+    global $post;
+     // Use nonce for verification to secure data sending
+     wp_nonce_field( basename( __FILE__ ), 'email_nonce' );
+    ?>
     <input type="email" name="email" id="email">
 <?php }
+
+function sucplugin_save_metabox_field($post_id){
+    // Verify nonce
+    if (!isset($_POST['email_nonce']) || !wp_verify_nonce($_POST['email_nonce'], basename(__FILE__))) {
+        return $post_id;
+    }
+
+      // check autosave
+        if ( wp_is_post_autosave( $post_id ) )
+        return 'autosave';
+
+        //check post revision
+        if ( wp_is_post_revision( $post_id ) )
+        return 'revision';
+
+        // check permissions
+        if ( 'project' == $_POST['post_type'] ) {
+        if ( ! current_user_can( 'edit_page', $post_id ) )
+            return 'cannot edit page';
+        } elseif ( ! current_user_can( 'edit_post', $post_id ) ) {
+            return 'cannot edit post';
+        }
+
+        $email_inserted = $_POST['email'];
+        update_post_meta($post_id, 'email', $email_inserted);
+
+         //simply we have to save the data now
+            global $wpdb;
+
+            $table = $wpdb->base_prefix . 'project_bids_mitglied';
+
+            $wpdb->insert(
+                        $table,
+                        array(
+                            'col_post_id' => $post_id, //as we are having it by default with this function
+                            'col_value'   => $email_inserted  //pass the email's string
+                        ),
+                        array(
+                            '%d', //%s - string, %d - integer, %f - float
+                            '%s', //%s - string, %d - integer, %f - float
+                        )
+                    );
+
+}
+add_action( 'save_post', 'sucplugin_save_metabox_field' );
+add_action( 'new_to_publish', 'sucplugin_save_metabox_field' );
 ?>
