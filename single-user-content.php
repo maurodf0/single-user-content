e6c783dc95c9d94cfce3c42840c51a60d3bc97c3
Mauro <maurodefalco96@gmail.com>
2024-07-02 23:01:39 +0200

    add description and exit

--- single-user-content.php
+++ single-user-content.php
@@ -2,10 +2,13 @@
 /*
 Plugin Name: Single User Content
 Author: Mauro De Falco
+Description: Create a private page for single user registered on your site. Only the designed user can see the post, fast, secure, free.
 Version: 1.0
 Text Domain: sucplugin
 Domain Path: /languages
 */
+
+if( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
 
 class SingleUserContent {
     function __construct() {
